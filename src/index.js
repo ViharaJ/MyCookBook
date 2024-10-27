0f4c004b236e6bdb42932db79755a997535dcef2
ViharaJ <vihara@live.ca>
2024-10-27 13:07:32 -0700

    include browserrouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import RecPage from './recPage';
 import InsertPage from './insertPage';
-
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import Landing from './LandingPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <RecPage/> */}
-    <InsertPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Landing/>}/>
+        <Route path ="create" element={<InsertPage/>}/>
+        <Route path ="rec/:id" element={<RecPage/>}/>
+      </Routes>
+     
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
---
+++ src/navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <nav>
+            <h1> My Recipe Book</h1>
+            <a href="/">Home</a>
+            <a href="/create">New Recipe</a>
+        </nav>
+    );
+}
